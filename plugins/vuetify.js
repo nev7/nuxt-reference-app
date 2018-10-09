580f235f3fb1991922c839a506d2d022f44d80bc
Neven Hristov <neven.hristov@covomo.de>
2018-10-09 12:13:04 +0200

    new colors

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,12 +4,21 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#121212', // a color that is not in the material colors palette
-    accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
-    warning: colors.amber.base,
-    error: colors.deepOrange.accent4,
-    success: colors.green.accent3
+  //   primary: '#121212', // a color that is not in the material colors palette
+  //   accent: colors.grey.darken3,
+  //   secondary: colors.amber.darken3,
+  //   info: colors.teal.lighten1,
+  //   warning: colors.amber.base,
+  //   error: colors.deepOrange.accent4,
+  //   success: colors.green.accent3
+  // },
+  // {
+    primary: "#03A9F4",
+    secondary: "#40C4FF",
+    accent: "#01579B",
+    error: "#F44336",
+    warning: "#ffeb3b",
+    info: "#2196f3",
+    success: "#4caf50"
   }
 })
